b006d7dfeb6a1d8bb1060777493d2f168cab6d42
Kevin <khom415@gmail.com>
2021-04-20 23:49:53 -0700

    Added html parser to miata page

--- checker.py
+++ checker.py
@@ -1,8 +1,17 @@
 import csv
 import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
+#List of sample urls 
 miata_url = "https://sfbay.craigslist.org/d/for-sale/search/sss?query=miata&sort=rel"
 roadbike_50cm_url="https://sfbay.craigslist.org/d/for-sale/search/sss?query=road%20bike%2050cm&sort=rel"
-roadbike_52cm_url="road bike 52cm"
+roadbike_52cm_url="https://sfbay.craigslist.org/search/sss?query=road+bike+52cm&sort=rel&purveyor-input=all"
+
+miata_page = urlopen(miata_url)
+html_decode_miata = miata_page.read().decode("utf-8")
+soup = BeautifulSoup(html_decode_miata, "html.parser")
+
+print(soup.get_text())
 
 
