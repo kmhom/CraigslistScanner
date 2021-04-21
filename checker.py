47369e8b4b94caf9830b83fb153f10d155d86ebf
Kevin <khom415@gmail.com>
2021-04-20 19:26:55 -0700

    Imported re and added sample urls

---
+++ checker.py
@@ -0,0 +1,8 @@
+import csv
+import re
+
+miata_url = "https://sfbay.craigslist.org/d/for-sale/search/sss?query=miata&sort=rel"
+roadbike_50cm_url="https://sfbay.craigslist.org/d/for-sale/search/sss?query=road%20bike%2050cm&sort=rel"
+roadbike_52cm_url="road bike 52cm"
+
+
